5deace63cfca62d911d5b8f4d44445d8427a4d44
David Gutman <dagutman@gmail.com>
2021-05-18 17:23:35 -0400

    working on histogram and simple thumb viewer

--- synapseSync/syncMetadataToMVP.py
+++ synapseSync/syncMetadataToMVP.py
@@ -2,8 +2,8 @@
 import girder_client
 import secrets as s
 import sys, os,json
-import time
-
+import time,tqdm
+import urllib.parse
 ### Adding girder_utils path
 sys.path.insert(0,"../loadBucketData")
 import girder_utils as gu
@@ -22,6 +22,16 @@ with open("./testData/image-rel1-metadata-20210426-153033.json","r") as fp:
 
 htanMetaToFile = { os.path.basename(m['Bucket_url']): m for m in htanMeta}
 
+def createDefaultThumbUrl( itemInfo ):
+	#### This will use a heuristic to set a spiffy thumbnail url for a mltichannel image
+	
+	simpleThumb = { 'bands': [\
+		{'frame':0, 'palette':['#000000','#0000ff'], 'min': 100, 'max': 5000},
+		{'frame':1,'palette':['#000000','#00ff00'], 'max': 5000},
+		{'frame':2,'palette':['#000000','#ff0000'], 'max': 5000}]}
+	return simpleThumb
+
+
 def createLargeImage(gc,itemId,force=True):
 	### Hit the endpoint to create a largeImage, this defaults
 	## To using the force option to transcode images
@@ -29,7 +39,7 @@ def createLargeImage(gc,itemId,force=True):
 	gc.post("item/%s/tiles?force=%s" % (itemId,force))
 
 needsLargeImage = 0
-createLargeImages = True
+createLargeImages = False
 addedMeta = 0
 ## I may not want to try and generate largeImages for every file
 ## and/or I may want to add additional logic to detect failed files
@@ -38,8 +48,6 @@ def formatChannelData( internal_metadata ):
 	### Reformmating internal channel metadata for the omeSceneDescription tag
 	### that the DSA uses for rendering scenes
 	if 'frames' in internal_metadata or 'channelmap' in internal_metadata:
-		#print(internal_metadata['frames'])
-
 		imgChannelList = []
 
 		for f in internal_metadata['frames']:
@@ -57,12 +65,12 @@ def formatChannelData( internal_metadata ):
 		return imgChannelList
 	return None
 
-
 problemItems = []
-
 itemData = []
 
-for i in itemSet:
+
+
+for i in tqdm.tqdm(itemSet):
 	itemData.append(i)
 	if 'largeImage' not in i:
 		needsLargeImage+=1
@@ -72,6 +80,48 @@ for i in itemSet:
 			if i['name'] in htanMetaToFile:
 				gc.addMetadataToItem(i['_id'],{'htanMeta': htanMetaToFile[i['name']]})
 				addedMeta +=1
+		if 'ioparams' not in i['meta']:
+			gc.addMetadataToItem(i['_id'],{'ioparams':{"Place":"Holder"}})
+
+			print("Analyizing %s %s" % (i['name'],i['_id']))
+			ioparams = {}
+			tileMetadata = gc.get("item/%s/tiles" % i['_id'])
+#			print(tileMetadata)
+			if 'frames' not in tileMetadata:
+				ioparams['frameCount'] = 0
+			else:
+				ioparams['frameCount'] = len(tileMetadata['frames'])
+
+			if 'channelmap' in tileMetadata:
+				ioparams['channelmap'] = tileMetadata['channelmap']
+
+#			print("Now trying to get the histogram... for ",i['name'],i['_id'])
+			hist = gc.get("item/%s/tiles/histogram?frame=0" % i['_id'])[0]
+#			print("hist returned")
+
+			ioparams['min'] = hist['min']
+			ioparams['max'] = hist['max']
+			ioparams['range'] = hist['range']
+			ioparams['hist'] = hist
+			gc.addMetadataToItem(i['_id'],{'ioparams':ioparams})
+
+		else:
+			##So we have an ioparams let's check the default thumburl?
+			#print("Found ioparams...")
+			ioparams = i['meta']['ioparams']
+			#print(ioparams['max'],ioparams['frameCount'])
+			if ('frameCount' in ioparams and ioparams['frameCount'] >3 ) :
+
+#			if(ioparams['frameCount']>3):
+				#print("Multichannel...")
+				thumbSettings = createDefaultThumbUrl(ioparams)
+				thumbnailString =  urllib.parse.quote_plus(json.dumps(thumbSettings))
+
+				ioparams['thumbnailUrl'] = thumbnailString
+				#print(ioparams['thumbnailUrl'])
+				gc.addMetadataToItem(i['_id'],{'ioparams':ioparams})
+
+
 		if 'omeSceneDescription' not in i['meta']:
 
 			try:
@@ -102,6 +152,12 @@ dsaCrossWalk =  {x['meta']['htanMeta']['SynapseID']:{ 'dsaId': x['_id'],
 
 with open("mvpImageData.working.json","w") as fp:
 	json.dump(dsaCrossWalk,fp)
+
+
+with open("mvpImageData.thumbnail.json","w") as fp:
+	json.dump(itemData
+
+
 
 
 ## imageBitDepth
