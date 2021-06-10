7532f32dae265b1b917edce6bb73575d53e51b4b
David Gutman <dagutman@gmail.com>
2021-06-10 09:02:51 -0400

    working on default thumb generator

--- synapseSync/updateDefaultThumbURL.py
+++ synapseSync/updateDefaultThumbURL.py
@@ -2,18 +2,15 @@
 import girder_utils as gu
 import girder_client
 import secrets as s
-import sys
-import os
-import json
-import time
-import tqdm
+import sys, os, json, time, tqdm
 import urllib.parse
 # Adding girder_utils path
+sys.path.insert(0, "../loadBucketData")
 
 updateMetadata = False
 
-sys.path.insert(0, "../loadBucketData")
 
+## Connect to the girder instance -- the apiToken is in the secrets file
 gc = girder_client.GirderClient(apiUrl=s.apiUrl)
 gc.authenticate(apiKey=s.dsaApiKey)
 
@@ -26,14 +23,12 @@ itemSet = gu.listChildItems(
 
 def createDefaultThumbUrl(itemInfo):
     # This will use a heuristic to set a spiffy thumbnail url for a mltichannel image
-
     simpleThumb = {'bands': [
         {'frame': 0, 'palette': ['#000000', '#0000ff'],
             'min': 'auto', 'max': 'auto'},
         {'frame': 1, 'palette': ['#000000', '#00ff00'], 'max': 'auto'},
         {'frame': 2, 'palette': ['#000000', '#ff0000'], 'max': 'auto'}]}
     return simpleThumb
-
 
 def formatChannelData(internal_metadata):
     # Reformmating internal channel metadata for the omeSceneDescription tag
@@ -174,73 +169,5 @@ for i in itemData:
 # Imaging_Assay_Type  HTAN_Center
 
 
-# 		if 'ioparams' not in i['meta']:
-# 			gc.addMetadataToItem(i['_id'], {'ioparams': {"Place": "Holder"}})
-
-# 			print("Analyizing %s %s" % (i['name'], i['_id']))
-# 			ioparams = {}
-# 			tileMetadata = gc.get("item/%s/tiles" % i['_id'])
-# #			print(tileMetadata)
-# 			if 'frames' not in tileMetadata:
-# 				ioparams['frameCount'] = 0
-# 			else:
-# 				ioparams['frameCount'] = len(tileMetadata['frames'])
-
-# 			if 'channelmap' in tileMetadata:
-# 				ioparams['channelmap'] = tileMetadata['channelmap']
-
-# #			print("Now trying to get the histogram... for ",i['name'],i['_id'])
-# 			hist = gc.get("item/%s/tiles/histogram?frame=0" % i['_id'])[0]
-# #			print("hist returned")
-
-# 			ioparams['min'] = hist['min']
-# 			ioparams['max'] = hist['max']
-# 			ioparams['range'] = hist['range']
-# 			ioparams['hist'] = hist
-# 			if updateMetadata: gc.addMetadataToItem(i['_id'], {'ioparams': ioparams})
-
-# 		else:
-# 			# So we have an ioparams let's check the default thumburl?
-# 			# print("Found ioparams...")
-# 			ioparams = i['meta']['ioparams']
-# 			# print(ioparams['max'],ioparams['frameCount'])
-# 			if ('frameCount' in ioparams and ioparams['frameCount'] > 3):
-
-# #			if(ioparams['frameCount']>3):
-# 				# print("Multichannel...")
-# 				thumbSettings = createDefaultThumbUrl(ioparams)
-# 				thumbnailString = urllib.parse.quote_plus(json.dumps(thumbSettings))
-
-# 				ioparams['thumbnailUrl'] = thumbnailString
-# 				# print(ioparams['thumbnailUrl'])
-# 				if updateMetadata: gc.addMetadataToItem(i['_id'], {'ioparams': ioparams})
-
 # 		if 'omeSceneDescription' not in i['meta']:
 
-# 			try:
-# 				tileMetadata = gc.get("item/%s/tiles" % i['_id'])
-# 				md = formatChannelData(tileMetadata)
-# 				if md:
-# 					print("Got some frame data!")
-# 					if updateMetadata: gc.addMetadataToItem(i['_id'], {'omeSceneDescription': md})
-# 			except:
-# 				problemItems.append(i)
-# 				print("Could not load metadata for %s" % i['_id'])
-# Clearing largeimage and regenerating
-# gc.delete("item/%s/tiles" % i['_id'])
-# time.sleep(0.5)
-# gc.post("item/%s/tiles" % i['_id'])
-
-# print(tileMetadata)
-#			break
-
-# print("Creating large image for %d images and added metadata to %d images" %
-#       (needsLargeImage, addedMeta))
-
-
-dsaRootUrl = "https://imaging.htan.dev/girder/api/v1/"
-
-
-# Pushing over the thumbnail data as an array
-# with open("mvpImageData.thumbnailV2.json", "w") as fp:
-# 	json.dump(itemData, fp)
